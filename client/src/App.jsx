0b8ac820b8e7f001f25947e3fd8dfb1017ca9f5e
Dany <103103303+danialfach@users.noreply.github.com>
2024-01-07 13:22:27 +0700

    Create Headr component, Create Server, Connect to Database

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import SignOut from "./pages/SignOut"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<SignOut  />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+  </BrowserRouter>
+  )
+}
